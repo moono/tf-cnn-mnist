de685b59fa8c81bdb10d37a71dd7cdf0deef094c
Mookyung Song <toilety@gmail.com>
2018-06-30 21:47:05 +0900

    clean up code

--- estimator_within_estimator.py
+++ estimator_within_estimator.py
@@ -1,8 +1,10 @@
 import os
-import shutil
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import predictor
+
+from helper import data_input_fn
+
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -13,9 +15,10 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 # load pretrained model
-pretrained_model_dir = './models/1530189906'
-another_graph = tf.Graph()
-predict_fn = predictor.from_saved_model(export_dir=pretrained_model_dir, graph=another_graph)
+pretrained_model_dir = './models/high_api/1530356406'
+# another_graph = tf.Graph()
+# predict_fn = predictor.from_saved_model(export_dir=pretrained_model_dir, graph=another_graph)
+predict_fn = predictor.from_saved_model(export_dir=pretrained_model_dir)
 
 
 def pretrained_predictor(x):
@@ -83,56 +86,15 @@ def cnn_model_fn(features, labels, mode, params):
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_ops)
 
 
-def parse_tfrecord(raw_record):
-    keys_to_features = {
-        'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-        'image/class/label': tf.FixedLenFeature((), tf.int64),
-    }
-
-    # parse feature
-    parsed = tf.parse_single_example(raw_record, keys_to_features)
-
-    label = tf.cast(parsed['image/class/label'], tf.int32)
-
-    # parse image
-    image = tf.image.decode_png(parsed['image/encoded'])
-    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-
-    return image, label
-
-
-def data_input_fn(data_fn, n_images, is_training, batch_size, epochs):
-    dataset = tf.data.TFRecordDataset(data_fn)
-
-    if is_training:
-        dataset = dataset.shuffle(buffer_size=n_images)
-
-    dataset = dataset.map(parse_tfrecord)
-    dataset = dataset.prefetch(batch_size)
-    dataset = dataset.repeat(epochs)
-    dataset = dataset.batch(batch_size)
-
-    iterator = dataset.make_one_shot_iterator()
-    images, labels = iterator.get_next()
-
-    features = {
-        'x': images,
-    }
-    return features, labels
-
-
-def train():
-
-    # assert predict_fn.graph is tf.get_default_graph()
-
+def train(model_dir):
     # load mnist data
-    train_dataset_fn_list = ['./data/mnist-train-00.tfrecord', './data/mnist-train-01.tfrecord']
-    eval_dataset_fn_list = ['./data/mnist-val-00.tfrecord', './data/mnist-val-01.tfrecord']
-    n_train_images = 55000
-    n_eval_images = 10000
+    mnist_tfrecord_dir = './data/mnist-tfrecord'
+    training_fn_list = ['mnist-train-00.tfrecord', 'mnist-train-01.tfrecord']
+    validate_fn_list = ['mnist-val-00.tfrecord', 'mnist-val-01.tfrecord']
+    training_fn_list = [os.path.join(mnist_tfrecord_dir, fn) for fn in training_fn_list]
+    validate_fn_list = [os.path.join(mnist_tfrecord_dir, fn) for fn in validate_fn_list]
 
     # hyper parameters
-    new_model_dir = './models_new'
     batch_size = 100
     epochs = 20
 
@@ -142,21 +104,18 @@ def train():
     # create the Estimator
     mnist_classifier = tf.estimator.Estimator(
         model_fn=cnn_model_fn,
-        model_dir=new_model_dir,
+        model_dir=model_dir,
         config=run_config,
         params={
             'input_size': 28,
             'n_output_classes': 10,
-            # 'pretrained_model': pretrained_mnist_classifier,
-            # 'model_a_predict_fn': predict_fn
-            # 'pretrained_model_dir': pretrained_model_dir,
         },
         warm_start_from=None
     )
 
     # train model
     mnist_classifier.train(
-        input_fn=lambda: data_input_fn(train_dataset_fn_list, n_train_images, True, batch_size, epochs),
+        input_fn=lambda: data_input_fn(training_fn_list, True, batch_size, epochs),
         hooks=None,
         steps=None,
         max_steps=None
@@ -165,45 +124,43 @@ def train():
     # evaluate the model and print results
     # hooks not working for evaluation?
     eval_results = mnist_classifier.evaluate(
-        input_fn=lambda: data_input_fn(eval_dataset_fn_list, n_eval_images, False, 1, 1))
+        input_fn=lambda: data_input_fn(validate_fn_list, False, 1, 1))
     print(eval_results)
 
     return
 
 
-def test_pretrained():
-    # ==========================================================================
-    # There will be pretrained model in './models/1524893870'
-    # here pretrained model is from 'higher_api.py' -> create_serving_model()
+def test_pretrained(model_servable_dir):
+    from tensorflow.examples.tutorials.mnist import input_data
+
+    # =============================================================================
+    # here pretrained model is from '4_high_level_api.py' -> create_serving_model()
     # the pretrained model will have input key as 'x' and output key as 'output'
-    # ==========================================================================
+    # =============================================================================
 
     # load pretrained model
-    pretrained_model_dir = './models/1524893870'
-    predict_fn = predictor.from_saved_model(export_dir=pretrained_model_dir)
+    pretrained_predict_fn = predictor.from_saved_model(export_dir=model_servable_dir)
 
-    # load test mnist data
-    mnist = tf.contrib.learn.datasets.load_dataset('mnist')
-    val_images = mnist.test.images
-    val_labels = mnist.test.labels
-    val_images = np.reshape(val_images, newshape=(-1, 28, 28, 1))
+    # load mnist data
+    mnist = input_data.read_data_sets('./data/mnist')
+    test_images = np.reshape(mnist.test.images, newshape=[-1, 28, 28, 1])
+    test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
     # lets predict just 10 images
-    predictions = predict_fn(
+    predictions = pretrained_predict_fn(
         {
-            'x': val_images[:10, :, :, :]
+            'x': test_images[:10, :, :, :]
         }
     )
     print(predictions['output'])    # outputs: [7 2 1 0 4 1 4 9 5 9]
-    print(val_labels[:10])          # outputs: [7 2 1 0 4 1 4 9 5 9]
+    print(test_labels[:10])         # outputs: [7 2 1 0 4 1 4 9 5 9]
     return
 
 
-def inspect_model_dir():
+def inspect_model_dir(model_checkpoint_dir):
     from tensorflow.python.tools import inspect_checkpoint as chkp
 
-    model_dir = './models'
-    ckpt_prefix = tf.train.latest_checkpoint(model_dir)
+    ckpt_prefix = tf.train.latest_checkpoint(model_checkpoint_dir)
 
     # print all tensors in checkpoint file
     chkp.print_tensors_in_checkpoint_file(ckpt_prefix, tensor_name='', all_tensors=True)
@@ -211,15 +168,18 @@ def inspect_model_dir():
 
 
 def main():
-    # # 0. inspect pretrained model dir
-    # inspect_model_dir()
+    # 0. inspect pretrained model dir
+    model_checkpoint_dir = './models/high_api'
+    inspect_model_dir(model_checkpoint_dir)
 
-    # # 1. test pretrained model
-    # test_pretrained()
+    # 1. test pretrained model
+    model_servable_dir = './models/high_api/1530356406'
+    test_pretrained(model_servable_dir)
 
     # 2. train the new model which takes pretrained model
     #    since this is toy example, pretrained model is mnist classifier and new model is same mnist classifier
-    train()
+    model_dir = './models/transfered'
+    train(model_dir)
     return
 
 
